b57278d8c3d6f9b201c4dfe375edca65003e3e50
pberu8531 <pberu8531@gmail.com>
2025-11-24 14:02:08 +0300

    Merge sorted list v2:draft merge function

--- 21E_mergeSortedList.js
+++ 21E_mergeSortedList.js
@@ -37,18 +37,58 @@ class LinkedList{
         }
 
 
-        if(this.head === null) this.head = ListNode(input[0])
+        if(this.head === null) this.head = new ListNode(input[0])
 
         let current = this.head
 
         while(current.next !== null) current = current.next
 
         for( let i = 1; input.length > i ; i ++){
-            current.next = ListNode( input[i] )
+            current.next = new ListNode( input[i] )
             current = current.next
         }
 
         return this.head
 
     }
+
+    printList(){
+
+        if( this.head === null ) return ""
+
+        let current = this.head
+        let output = ""
+
+        while(current){
+            output += `${current.val}->`
+            current = current.next 
+        }
+
+        output += "NULL"
+
+        return output
+
+    }
+
 }
+
+const list_1 = new LinkedList(null)
+list_1.createList([1,2,4])
+const list_2 = new LinkedList(null)
+list_2.createList([1,3,4])
+
+function mergeSortedList( list1, list2 ){
+    
+    // Plan
+    // 1. check if any  or both of the lists are null, if one is null return the other.
+    // 2. If both are empty return null
+    // 3. Use 2 pointers each starting at the head of each list.
+    // 4. Create an empty Linked list.
+    // 5. Check the curent val of each pointer.
+    // 6. Put the lesser val as the next node 
+
+
+}
+
+console.log("111", list_1.printList() )
+console.log("222", list_2.printList() )
