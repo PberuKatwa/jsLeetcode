ff97e4e1d7f6b0c52c89b9983639549aada6ae28
pberu8531 <pberu8531@gmail.com>
2025-11-24 13:11:46 +0300

    Merge sorted list v1:print linked list

--- 21E_mergeSortedList.js
+++ 21E_mergeSortedList.js
@@ -18,76 +18,37 @@
 // Input: list1 = [], list2 = [0]
 // Output: [0]
 
-/**
- * @class ListNode
- */
-
 class ListNode{
-    /**
-     * @param {number} val
-     */
-    constructor(val = null){
+    constructor( val=null ){
         this.val = val
         this.next = null
     }
 }
 
 class LinkedList{
-    
-    constructor(head = null){
+    constructor( head = null){
         this.head = head
     }
 
-    createFromArray(list){
+    createList(input){
 
-        if(list.length < 1){
+        if(input.length === 0){
             return this.head
         }
 
-        if(this.head === null) this.head = new ListNode(list[0])
-            
+
+        if(this.head === null) this.head = ListNode(input[0])
+
         let current = this.head
-        // console.log("vall", current.val)
 
-        while( current.next ) { 
-            // console.log("current", current.val)
-            current = current.next 
-        }
+        while(current.next !== null) current = current.next
 
-        for( let i=1; i < list.length ;i++ ){
-            // if(current.val!== null && current.val !== undefined){
-            //     console.log("vall", current.val)
-            // }
-            // console.log("nodee", list[i])
-            current.next = new ListNode(list[i])
+        for( let i = 1; input.length > i ; i ++){
+            current.next = ListNode( input[i] )
             current = current.next
         }
 
         return this.head
 
     }
-
-    printList(){
-
-        if(!this.head) return ""
-
-        let output = ""
-        let current = this.head
-        console.log("curr vall", this.head.val)
-
-        while( current ){
-            console.log( "list", current.val )
-            output += `${current.val}->`
-            current = current.next
-        }
-
-        output +="NULL"
-        return output
-    }
-
-
 }
-
-const head1 = new LinkedList(null)
-head1.createFromArray([1,2,4])
-console.log("111", head1.printList())
