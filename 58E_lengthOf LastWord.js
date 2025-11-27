0ccb13383e95a38d9902a891d37d5d19032aef98
pberu8531 <pberu8531@gmail.com>
2025-11-27 10:27:27 +0300

    Length of last word v2:first test case

--- 58E_lengthOf LastWord.js
+++ 58E_lengthOf LastWord.js
@@ -41,10 +41,16 @@ function lengthLastWord(s){
     const sLength = ( s.length ) - 1
     let count = 0
 
-    for( let i = sLength ; i < s.length ; i --  ){
+    for( let i = sLength ; i >= 0 ; i --  ){
+
+        if( s[i] === " " && count > 0 ) return count
+
+        if( s[i] !== " " ) count += 1;
 
     }
 
     return count
     
 }
+
+console.log( "111", lengthLastWord("Hello World") )
