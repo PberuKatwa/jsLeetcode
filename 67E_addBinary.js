ef784fa0a0b5c365604b6b6d32ec7488b3c8c049
pberu8531 <pberu8531@gmail.com>
2025-12-17 11:53:49 +0300

    Add binary v3:set plan

--- 67E_addBinary.js
+++ 67E_addBinary.js
@@ -12,3 +12,10 @@
  * @param {string} a
  * @param {string} b
  */
+
+function addBinary(a,b){
+    // Plan
+    // 1. Check if either a or b is null, if either is null return the non null value.
+    // 2. If both are null return null.
+    // 3. Find the length of both a and b.
+}
