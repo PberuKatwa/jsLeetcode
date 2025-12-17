db4b072bd653a0feaeba7ef02b35f983d9688d59
pberu8531 <pberu8531@gmail.com>
2025-12-17 12:02:04 +0300

    Add binary v3:full plan

--- 67E_addBinary.js
+++ 67E_addBinary.js
@@ -18,4 +18,14 @@ function addBinary(a,b){
     // 1. Check if either a or b is null, if either is null return the non null value.
     // 2. If both are null return null.
     // 3. Find the length of both a and b.
+    // 4. Initialize  a sum variable and carry vaiable(let sum & let carry).
+    // 5. Loop from the last index of a and b.
+    // 6. Scenario 1 summation, 0 + 0 = 0. Sum is 0 and carry is 0.
+    // 7. Scenario 2 summation, 1 + 0 = 0. Sum is 1 and carry is 0. 
+    // 8. Scenario 3 summation, 1 + 1 = 0. Sum is 0 and carry is 1.
+    // 9. Scenario 4 summation, 1 + 1 + 1(carry) = 0. Sum is 1 and carry is 1.
+    // 10. Scenario 5 summation, 1 + 0 + 1(carry) = 0. Sum is 0 and carry is 1.
+    // 11. Scenario 6 summation, 1 + 0 + 0(carry) = 0. Sum is 1 and carry is 0. 
+ 
+ 
 }
