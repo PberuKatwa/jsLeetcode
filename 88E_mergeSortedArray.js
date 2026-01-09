1f0cb470f5de9542ec5e123ecb05ef685557dd2d
pberu8531 <pberu8531@gmail.com>
2026-01-09 11:47:57 +0300

    88 Merge Sorted Array v14:created draft cases

--- 88E_mergeSortedArray.js
+++ 88E_mergeSortedArray.js
@@ -89,8 +89,8 @@ function mergeSortedArray(nums1, nums2, m, n){
 cases = [
     { nums1:[0], m:0, nums2:[], n:0, expected:[] },
     { nums1:[0], m:0, nums2:[2], n:1, expected:[2] },
-    { nums1:[1], m:1, nums2:[1], n:0, expected:[1] },
-    { nums1:[1,2,3,0,0,0], m:3, nums2:[2], n:1, expected:[1,2,2,3,0,0] },
+    { nums1:[1], m:1, nums2:[], n:0, expected:[1] },
+    { nums1:[1,2,3], m:3, nums2:[2], n:1, expected:[1,2,2,3] },
     { nums1:[1,0,0,0,0,0], m:1, nums2:[0,0,1,2,5], n:5, expected:[0,0,1,1,2,5] },
     { nums1:[1,1,1,1,0,0,0,0], m:4, nums2:[1,1,1,1], n:4, expected:[1,1,1,1,1,1,1,1] },
     { nums1:[8,9,9,0,0,0], m:3, nums2:[2,5,6], n:3, expected:[2,5,6,8,9,9] }
