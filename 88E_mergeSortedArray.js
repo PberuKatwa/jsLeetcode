a1d610a22960bec1418ab410ae0db22c32d643ae
pberu8531 <pberu8531@gmail.com>
2026-01-09 11:30:00 +0300

    88 Merge Sorted Array v10:created draft functions

--- 88E_mergeSortedArray.js
+++ 88E_mergeSortedArray.js
@@ -61,4 +61,25 @@ function mergeSortedArray(nums1, nums2, m, n){
     if( m<1 ) return nums2;
     if( n<1 ) return nums1;
 
+    let p1 = m-1
+    let p2 = n-1
+    let p3 = ( m+n ) - 1
+
+    while( p1>=0 || p2>=0 ){
+
+        if( nums2[p2] >= nums1[p1] ){
+
+            nums1[p3] =  nums2[p2]
+            p2 -= 1
+
+        }else{
+            nums1[p3] = nums1[p1]
+            p1 -= 1
+        }
+
+        p3 -= 1
+
+    }
+
+    return nums1
 }
