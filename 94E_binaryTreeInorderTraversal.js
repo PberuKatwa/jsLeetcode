320adbffb78ecb8c67837eec08c671088abe0dfb
pberu8531 <pberu8531@gmail.com>
2026-01-14 17:22:08 +0300

    94E binary tree in order v2:created tree node

--- 94E_binaryTreeInorderTraversal.js
+++ 94E_binaryTreeInorderTraversal.js
@@ -21,3 +21,11 @@
 // The number of nodes in the tree is in the range [0, 100].
 // -100 <= Node.val <= 100
 // Follow up: Recursive solution is trivial, could you do it iteratively?
+
+class TreeNode{
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
