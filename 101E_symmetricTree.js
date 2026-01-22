4b6e166bf8c8adea08476c40bda73bf253760579
Pberu <pberu8531@gmail.com>
2026-01-22 13:36:59 +0300

    101E symmetric tree v2:set question

--- 101E_symmetricTree.js
+++ 101E_symmetricTree.js
@@ -8,11 +8,8 @@
 // Input: root = [1,2,2,null,3,null,3]
 // Output: false
 
-
 // Constraints:
-
 // The number of nodes in the tree is in the range [1, 1000].
 // -100 <= Node.val <= 100
-
 
 // Follow up: Could you solve it both recursively and iteratively?
