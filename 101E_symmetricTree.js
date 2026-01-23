c459a513150fcd053940b4148bd33a190896522c
Pberu <pberu8531@gmail.com>
2026-01-23 10:20:44 +0300

    101E symmetric data v8:almost complete build tree plan

--- 101E_symmetricTree.js
+++ 101E_symmetricTree.js
@@ -47,6 +47,16 @@ class BinaryTree{
     // 9.Assign the apparent right child by assigning the value at the current index + 1 in the input array(input[index + 1]),if its
     //    null then assign null as the right child.
     //
+    // CLARIFICATION: The reason we assign a null value to children is because this means that path is "dead" but we need to maintain
+    // the tree structure since were building level by level. If we failed the index would be out of sync since the input array
+    // is level order with "dead" paths being indicated as null. It is also true to say the child is null.
+    //
+    // 10.We check if the left child is not null.If its null we skip if not we assign it as the left child of the parent(parent.left = left).
+    //    After assigning we add the left child to the queue to be processed in another loop cycle.
+    // 11.We then check if the right child is not null.If its null we skip if not we assign it as the right child of the parent(parent.right = right).
+    //    After assigning we add the right child to the queue to be processed in another loop cycle.
+    // 12.The last action in the loop is incrementing the index by 2 (index += 2) since were done with this level.
+    //
     if (!input) return this.root;
   }
 
