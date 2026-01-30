baac6584702ea396bb4e5643c9473df75e1fbc87
Pberu <pberu8531@gmail.com>
2026-01-30 11:48:24 +0300

    104E max depth of symmetric tree v6:initialized print tree function

--- 104E_MaxDepthBinaryTree.js
+++ 104E_MaxDepthBinaryTree.js
@@ -1,8 +1,6 @@
 // Given the root of a binary tree, return its maximum depth.
 // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
-
-
 // Example 1:
 // Input: root = [3,9,20,null,null,15,7]
 // Output: 3
@@ -64,3 +62,8 @@ class BinaryTree{
     return this.root;
   }
 }
+
+const cases = [
+  { input: [3, 9, 20, null, null, 15, 7], output: [3, 9, 20, null, null, 15, 7] },
+  { input: [1,null,2], output: [1,null,2] },
+]
