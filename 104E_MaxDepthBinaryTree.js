46ccbdef0b6342ed07bb9d2c49579c8197dae53a
Pberu <pberu8531@gmail.com>
2026-02-02 11:40:31 +0300

    104E iterative solution v2:working build tree

--- 104E_MaxDepthBinaryTree.js
+++ 104E_MaxDepthBinaryTree.js
@@ -57,6 +57,7 @@ class BinaryTree{
       }
 
       index += 2;
+      console.log("\nindexx", index ,"queue", queue)
     }
     return this.root;
   }
@@ -87,62 +88,67 @@ class BinaryTree{
 
   maxDepthIterative() {
 
+    if (!this.root) return 0;
+
+    const root = this.root;
+
+
   }
 
 }
 
-const cases = [
-  { input: [3, 9, 20, null, null, 15, 7], expected: [3, 9, 20, null, null, 15, 7] },
-  { input: [1,null,2], expected: [1,null,2] },
-]
-
-cases.forEach(
-  function ({ input, expected }) {
-
-    const tree = new BinaryTree();
-    tree.buildTree(input);
-
-    const actual = tree.printTree();
-
-    const isMatch = (actual.length === expected.length) && (actual.every((value, index) => { value === expected[index] }));
-
-    if (!isMatch) {
-      console.log(`FAILED TEST for INPUT:${input} and ACTUAL:${actual} but EXPECTED${expected}`)
-    } else {
-      console.log(`SUCCESS TEST for INPUT:${input} with EXPECTED:${expected}`)
-    }
-
-  }
-)
-
-
-
 // const cases = [
-//   { input: [3, 9, 20, null, null, 15, 7], expected: 3 },
-//   { input: [1, null, 2], expected: 2 },
-//   { input: [1, 2, null, 3, null, 4], expected: 4 },
-//   { input: [1, 2, 3, 4, null, null, null], expected: 3 },
-//   { input: [1, 2, 3, 4, 5, null, null, 6], expected: 4 },
-//   { input: [1, 2, null, 3, 4, null, null, 5], expected: 4 },
-//   { input: [1, 2, null, 3, 4, 6, 7], expected: 4 },
-//   { input: [1, 2, null, 3, null, 4, null, 5], expected: 5 },
-//   { input: [1, 0, 0, 0], expected: 3 },
-//   { input: [1, 2, 3, 4, 5, 6, 7], expected: 3 }
-
-
+//   { input: [3, 9, 20, null, null, 15, 7], expected: [3, 9, 20, null, null, 15, 7] },
+//   { input: [1,null,2], expected: [1,null,2] },
 // ]
 
 // cases.forEach(
 //   function ({ input, expected }) {
 
 //     const tree = new BinaryTree();
-//     const { count } = tree.buildTree(input);
+//     tree.buildTree(input);
 
-//     if (count!==expected) {
-//       console.log(`FAILED TEST with INPUT:${input} and ACTUAL:${count} but EXPECTED:${expected}`);
+//     const actual = tree.printTree();
+
+//     const isMatch = (actual.length === expected.length) && (actual.every((value, index) => { value === expected[index] }));
+
+//     if (!isMatch) {
+//       console.log(`FAILED TEST for INPUT:${input} and ACTUAL:${actual} but EXPECTED${expected}`)
 //     } else {
-//       console.log(`SUCCESS TEST with INPUT:${input} and EXPECTED:${expected}`)
+//       console.log(`SUCCESS TEST for INPUT:${input} with EXPECTED:${expected}`)
 //     }
 
 //   }
 // )
+
+
+
+const cases = [
+  { input: [3, 9, 20, null, null, 15, 7], expected: 3 },
+  { input: [1, null, 2], expected: 2 },
+  { input: [1, 2, null, 3, null, 4], expected: 4 },
+  { input: [1, 2, 3, 4, null, null, null], expected: 3 },
+  { input: [1, 2, 3, 4, 5, null, null, 6], expected: 4 },
+  { input: [1, 2, null, 3, 4, null, null, 5], expected: 4 },
+  { input: [1, 2, null, 3, 4, 6, 7], expected: 4 },
+  { input: [1, 2, null, 3, null, 4, null, 5], expected: 5 },
+  { input: [1, 0, 0, 0], expected: 3 },
+  { input: [1, 2, 3, 4, 5, 6, 7], expected: 3 }
+
+
+]
+
+cases.forEach(
+  function ({ input, expected }) {
+
+    const tree = new BinaryTree();
+    const { count } = tree.buildTree(input);
+
+    if (count!==expected) {
+      console.log(`FAILED TEST with INPUT:${input} and ACTUAL:${count} but EXPECTED:${expected}`);
+    } else {
+      console.log(`SUCCESS TEST with INPUT:${input} and EXPECTED:${expected}`)
+    }
+
+  }
+)
