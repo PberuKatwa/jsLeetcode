2f6bc2422a4a09f288e083ddfa71369bbec383c5
Pberu <pberu8531@gmail.com>
2026-02-25 10:45:10 +0300

    Redo 110E v8:added tests

--- Redo/110E_balanced_binary_tree.js
+++ Redo/110E_balanced_binary_tree.js
@@ -100,6 +100,28 @@ class BinaryTree {
 
     return result;
   }
-
-
 }
+
+const cases = [
+  { input: [3, 9, 20, null, null, 15, 7], expected: [3, 9, 20, null, null, 15, 7] },
+  { input: [1, 2, 2, 3, 3, null, null, 4, 4], expected: [1, 2, 2, 3, 3, null, null, 4, 4] }
+
+]
+
+cases.forEach(
+  ({ input, expected })=>{
+
+  }
+)
+
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: true
+
+// Example 2:
+// Input: root = [1,2,2,3,3,null,null,4,4]
+// Output: false
+
+// Example 3:
+// Input: root = []
+// Output: true
