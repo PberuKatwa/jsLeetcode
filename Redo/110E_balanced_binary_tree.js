abca8427dc7f463ab38124f9ae1c604fe3ae4d2e
Pberu <pberu8531@gmail.com>
2026-02-26 11:16:48 +0300

    110 E redo v1:set function

--- Redo/110E_balanced_binary_tree.js
+++ Redo/110E_balanced_binary_tree.js
@@ -102,47 +102,65 @@ class BinaryTree {
   }
 
   checkBalanceRecursively() {
+    if (this.root) return null;
+    const result = this._balanceRecursively(this.root);
 
+    if (result === -1) {
+      return false
+    }
+
+    return true;
   }
 
   /**
    *
    * @param {TreeNode} node
    */
-  _balanceRecursively(node) {
-    if (!node) return null;
+   _balanceRecursively(node) {
+     if (!node) return 0;
+     console.log("height")
+     const leftHeight = this._balanceRecursively(node.left);
+     if (leftHeight === -1) return -1;
 
-    const left = this._balanceRecursively(node.left);
-    const right = this._balanceRecursively(node.right);
+     const rightHeight = this._balanceRecursively(node.right);
+     if (rightHeight === -1) return -1;
 
-    return 1 + left + right;
-  }
+     if (Math.abs(leftHeight - rightHeight) > 1) return -1;
 
+     return 1 + Math.max(leftHeight, rightHeight);
+   }
 }
 
+// const cases = [
+//   { input: [3, 9, 20, null, null, 15, 7], expected: [3, 9, 20, null, null, 15, 7] },
+//   { input: [1, 2, 2, 3, 3, null, null, 4, 4], expected: [1, 2, 2, 3, 3, null, null, 4, 4] }
+
+// ]
+
+
+// cases.forEach(
+//   ({ input, expected })=>{
+
+//     const tree = new BinaryTree();
+//     tree.buildTree(input);
+//     const actual = tree.printTree();
+
+//     const isMatch = (actual.length === expected.length) && (expected.every((value, index) => (value === actual[index])));
+
+//     if (!isMatch) {
+//       console.log(`FAILED TEST for INPUT:${input} with ACTUAL:${actual} and EXPECTED:${expected}`);
+//     } else {
+//       console.log(`SUCCESS TEST for INPUT:${input} and ACTUAL:${actual}`)
+//     }
+
+//   }
+// )
+
 const cases = [
-  { input: [3, 9, 20, null, null, 15, 7], expected: [3, 9, 20, null, null, 15, 7] },
-  { input: [1, 2, 2, 3, 3, null, null, 4, 4], expected: [1, 2, 2, 3, 3, null, null, 4, 4] }
-
+  { input: [3, 9, 20, null, null, 15, 7], expected: true },
+  { input: [1, 2, 2, 3, 3, null, null, 4, 4], expected: false },
+  { input: [], expected: true }
 ]
-
-cases.forEach(
-  ({ input, expected })=>{
-
-    const tree = new BinaryTree();
-    tree.buildTree(input);
-    const actual = tree.printTree();
-
-    const isMatch = (actual.length === expected.length) && (expected.every((value, index) => (value === actual[index])));
-
-    if (!isMatch) {
-      console.log(`FAILED TEST for INPUT:${input} with ACTUAL:${actual} and EXPECTED:${expected}`);
-    } else {
-      console.log(`SUCCESS TEST for INPUT:${input} and ACTUAL:${actual}`)
-    }
-
-  }
-)
 
 // Example 1:
 // Input: root = [3,9,20,null,null,15,7]
