a525a40f670726e2480d5f8aa7df229ff9a53eba
Pberu <pberu8531@gmail.com>
2026-03-11 21:45:51 +0300

    palindrome redo v2:added

--- Redo/9E_palindrome.js
+++ Redo/9E_palindrome.js
@@ -15,3 +15,16 @@
 // Input: x = 10
 // Output: false
 // Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+/**
+ * @param {number} x
+ */
+
+function isPalindrome(x) {
+
+  // Plan.
+  // 1. Check if the number is null, less than 0 and
+
+  if (!x | x < 0 | x % 10 === 0) return false;
+
+}
