0144960423fc89c2afdf73d7b0471af57319e650
Pberu <pberu8531@gmail.com>
2026-03-12 11:27:40 +0300

    14E valid parenthesis v5:added cases

--- Redo/20E_validParenthesis.js
+++ Redo/20E_validParenthesis.js
@@ -30,6 +30,7 @@
 
 /**
  * @param {string} s
+ * @returns {boolean}
  */
 function isParenthesisValid(s) {
 
@@ -54,3 +55,27 @@ function isParenthesisValid(s) {
 
   return true;
 }
+
+const cases = [
+  {input:"()",expected:true}
+]
+
+// Example 1:
+// Input: s = "()"
+// Output: true
+
+// Example 2:
+// Input: s = "()[]{}"
+// Output: true
+
+// Example 3:
+// Input: s = "(]"
+// Output: false
+
+// Example 4:
+// Input: s = "([])"
+// Output: true
+
+// Example 5:
+// Input: s = "([)]"
+// Output: false
